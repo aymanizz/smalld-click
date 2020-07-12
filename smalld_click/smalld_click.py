af460e6286b524762b4b1d0a3327cb85040c9694
Ayman Izzeldin <ayman.izzeldin@gmail.com>
2020-07-12 23:17:47 +0200

    feat: buffer calls to echo

--- smalld_click/smalld_click.py
+++ smalld_click/smalld_click.py
@@ -2,8 +2,8 @@ import contextlib
 import logging
 import shlex
 import threading
-from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
+from io import StringIO
 
 import click
 from pkg_resources import get_distribution
@@ -15,11 +15,11 @@ logger = logging.getLogger("smalld_click")
 
 
 class SmallDCliRunnerContext:
-    __slots__ = "runner", "message"
-
     def __init__(self, runner, message):
         self.runner = runner
         self.message = message
+        self.echo_buffer = StringIO()
+        self.buffered = True
 
 
 def get_runner_context():
@@ -62,6 +62,7 @@ class SmallDCliRunner:
             ctx = self.cli.make_context(self.cli.name, args, parent=parent_ctx)
             manager.enter_context(ctx)
             self.cli.invoke(ctx)
+            echo(flush=True, nl=False)
 
     def wait_for_message(self, msg):
         handle = Completable()
@@ -96,7 +97,7 @@ def managed_click_execution():
             pass
         except TimeoutError:
             pass
-        except Exception as e:
+        except:
             logger.exception("exception in command handler")
 
 
@@ -120,14 +121,29 @@ class Completable:
             return self._result
 
 
-def echo(message="", *args, **kwargs):
-    if not message:
+def echo(message=None, nl=True, file=None, *args, flush=False, **kwargs):
+    ctx = get_runner_context()
+
+    click_echo(message, file=ctx.echo_buffer, nl=nl, *args, **kwargs)
+    if ctx.buffered and not flush:
         return
 
-    ctx = get_runner_context()
-    runner, msg = ctx.runner, ctx.message
+    smalld, msg = ctx.runner.smalld, ctx.message
+    content = ctx.echo_buffer.getvalue()
+    ctx.echo_buffer = StringIO()
 
-    runner.smalld.post(f"/channels/{msg['channel_id']}/messages", {"content": message})
+    if not content:
+        return
+    smalld.post(f"/channels/{msg['channel_id']}/messages", {"content": content})
+
+
+def prompt(*args, **kwargs):
+    ctx = get_runner_context()
+
+    ctx.buffered = False
+    result = click_prompt(*args, **kwargs)
+    ctx.buffered = True
+    return result
 
 
 def visible_prompt(prompt="", *args, **kwargs):
@@ -135,9 +151,12 @@ def visible_prompt(prompt="", *args, **kwargs):
     runner, msg = ctx.runner, ctx.message
 
     if prompt:
-        echo(prompt)
+        echo(prompt, flush=True)
     return runner.wait_for_message(msg)
 
+
+click_echo = click.echo
+click_prompt = click.prompt
 
 click.echo = echo
 click.core.echo = echo
@@ -145,5 +164,9 @@ click.utils.echo = echo
 click.termui.echo = echo
 click.decorators.echo = echo
 click.exceptions.echo = echo
+
+click.prompt = prompt
+click.termui.prompt = prompt
+click.core.prompt = prompt
 
 click.termui.visible_prompt_func = visible_prompt
